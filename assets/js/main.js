c054f138c87df588ba5eb563ca566da59bbe3bd4
AIbnuHibban <abd.ibnuhibban@gmail.com>
2020-03-21 06:14:30 +0700

    to v.0.6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,41 @@
 $(window).on('load',function(){
     $('#welcome').modal('show');
 });
+document.addEventListener('contextmenu', event => event.preventDefault());
+$(document).keydown(function(e){
+    if(e.which === 123){
+       return false;
+    }else if ((event.ctrlKey && event.shiftKey && event.keyCode == 73) || (event.ctrlKey && event.shiftKey && event.keyCode == 74)) {
+        return false;
+    }else if (e.ctrlKey && e.keyCode == 85) {
+        return false;
+    }else if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+        disabledEvent(e);
+    }
+});
+$('textarea').keypress(function(event) {
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        generate()
+    }
+});
 function copyClipboard(jenis) {
     var clipboard = $(`#clipboard_${jenis}`);
-    clipboard.select();
-    document.execCommand("copy");
-    alert("berhasil");
+    if (clipboard.text() === "") {
+        Swal.fire(
+            'Oopss..',
+            `Please Fill In The Required Fields`,
+            'error'
+        )
+    }else{
+        clipboard.select();
+        document.execCommand("copy");
+        Swal.fire(
+            'Copy To Clipboard!',
+            `${jenis.charAt(0).toUpperCase() + jenis.substr(1)} Copied Successfully`,
+            'success'
+        )
+    }
 }
 
 function generate(){
@@ -19,55 +49,216 @@ function generate(){
     var cp_crud         = $('#clipboard_crud')
     var cp_migration    = $('#clipboard_migration')
     let kolom           = ""
-    for (let i = 0; i < list_column.length; i++) {
-        if (i === list_column.length-1) {
-            kolom += '"'+list_column[i]+'"'
-        }else{
-            kolom += '"'+list_column[i]+'",'
+    if (table.val() === "" || list_column_val === "") {
+        Swal.fire(
+            'Oopss..',
+            `Please Fill In The Required Fields`,
+            'error'
+        )
+    }else{
+        for (let i = 0; i < list_column.length; i++) {
+            if (i === list_column.length-1) {
+                kolom += '"'+list_column[i]+'"'
+            }else{
+                kolom += '"'+list_column[i]+'",'
+            }
         }
-    }
-    let validasi           = ""
-    for (let i = 0; i < list_column.length; i++) {
-        if (i === list_column.length-1) {
-            validasi += '"'+list_column[i]+'"         => "required"'
-        }else{
-            validasi += '"'+list_column[i]+'"         => "required",\n            '
+        let validasi           = ""
+        for (let i = 0; i < list_column.length; i++) {
+            if (i === list_column.length-1) {
+                validasi += '"'+list_column[i]+'"         => "required"'
+            }else{
+                validasi += '"'+list_column[i]+'"         => "required",\n            '
+            }
         }
-    }
-    let data           = ""
-    for (let i = 0; i < list_column.length; i++) {
-        if (i === list_column.length-1) {
-            data += `"${list_column[i]}"         => $request->${list_column[i]}`
-        }else{
-            data += `"${list_column[i]}"         => $request->${list_column[i]},\n            `
+        let data           = ""
+        for (let i = 0; i < list_column.length; i++) {
+            if (i === list_column.length-1) {
+                data += `"${list_column[i]}"         => $request->${list_column[i]}`
+            }else{
+                data += `"${list_column[i]}"         => $request->${list_column[i]},\n            `
+            }
         }
-    }
-    let data_comment           = ""
-    for (let i = 0; i < list_column.length; i++) {
-        if (i === list_column.length-1) {
-            data_comment += `//     "${list_column[i]}"         => $request->${list_column[i]}`
-        }else{
-            data_comment += `//     "${list_column[i]}"         => $request->${list_column[i]},\n        `
+        let data_comment           = ""
+        for (let i = 0; i < list_column.length; i++) {
+            if (i === list_column.length-1) {
+                data_comment += `//     "${list_column[i]}"         => $request->${list_column[i]}`
+            }else{
+                data_comment += `//     "${list_column[i]}"         => $request->${list_column[i]},\n        `
+            }
         }
-    }
-    let migrations           = ""
-    for (let i = 0; i < list_column.length; i++) {
-        if (i === list_column.length-1) {
-            migrations += `$table->string('${list_column[i]}');`
-        }else{
-            migrations += `$table->string('${list_column[i]}');\n            `
+        let migrations           = ""
+        for (let i = 0; i < list_column.length; i++) {
+            if (i === list_column.length-1) {
+                migrations += `$table->string('${list_column[i]}');`
+            }else{
+                migrations += `$table->string('${list_column[i]}');\n            `
+            }
         }
-    }
-    res_model.text(`<?php
+        res_model.text(`<?php
 
+    namespace App;
+        
+    use Illuminate\\Database\\Eloquent\\Model;
+        
+    class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Model{
+        protected $table = "${table.val().toLowerCase().replace(" ", "_")}";
+
+        protected $fillable = [${kolom}];
+
+        /**
+         * Create by LeeNuksID :D
+         *
+         * Thanks For Using Laragen
+         */
+    }`)
+        res_crud.text(`<?php
+
+
+    namespace App\\Http\\Controllers;
+
+    use Illuminate\\Http\\Request;
+    use App\\${table.val().charAt(0).toUpperCase() + table.val().substr(1)};
+    use Illuminate\\Support\\Facades\\Validator;
+    // use Illuminate\\Support\\Facades\\Auth;
+
+    class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller extends Controller{
+
+        public function store(Request $request){
+            $validate = Validator::make($request->all(),[
+                ${validasi}
+            ]);
+
+            if ($validate->fails()) {
+                return redirect()->back()->withErrors($validate);
+            }
+
+            $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create([
+                ${data}
+            ]);
+
+            // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::insert([
+            ${data_comment}
+            // ]);
+            
+            // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create($request->all());
+
+
+            if ($send) {
+                return redirect()->back();
+            }
+        }
+
+        public function update(Request $request,$${table.val().toLowerCase()}){
+            $validate = Validator::make($request->all(),[
+                ${validasi}
+            ]);
+
+            if ($validate->fails()) {
+                return redirect()->back()->withErrors($validate);
+            }
+
+            $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
+
+            $array = [
+                ${data}
+            ];
+
+            if ($${table.val().toLowerCase()}->update($array)) {
+                return redirect()->back();
+            }
+        }
+
+        public function destroy($${table.val().toLowerCase()}){
+            $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
+            if ($${table.val().toLowerCase()}->delete()) {
+                return redirect()->back();
+            }
+        }
+
+        /**
+         * Create by LeeNuksID :D
+         *
+         * Thanks For Using Laragen
+         */
+    }`)
+        res_migration.text(`<?php
+
+    use Illuminate\\Database\\Migrations\\Migration;
+    use Illuminate\\Database\\Schema\\Blueprint;
+    use Illuminate\\Support\\Facades\\Schema;
+
+    class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Migration{
+        
+        public function up(){
+            Schema::create('${table.val().toLowerCase().replace(" ", "_")}', function (Blueprint $table) {
+                
+                $table->bigIncrements('id');
+                ${migrations}
+                $table->timestamps();
+
+                // Options: See More at https://laravel.com/docs/5.8/migrations#creating-columns
+                // integer
+                // bigInteger
+                // binary
+                // boolean
+                // char
+                // date
+                // dateTime
+                // decimal
+                // double
+                // enum('level', ['easy', 'hard']);
+                // float
+                // longText
+            });
+        }
+
+        public function down(){
+            Schema::dropIfExists('${table.val().toLowerCase().replace(" ", "_")}');
+        }
+
+        /**
+         * Create by LeeNuksID :D
+         *
+         * Thanks For Using Laragen
+         */
+    }`)
+        
+        cp_model.text('')
+        cp_crud.text('')
+        cp_migration.text('')
+        cp_model.text(res_model.text())
+        cp_crud.text(res_crud.text())
+        cp_migration.text(res_migration.text())
+        list_column_val = "";
+    }
+}
+
+function example(){
+    var table           = "Example"
+    var res_model       = $('#result_models')
+    var res_crud        = $('#result_crud')
+    var res_migration   = $('#result_migration')
+    var cp_model        = $('#clipboard_models')
+    var cp_crud         = $('#clipboard_crud')
+    var cp_migration    = $('#clipboard_migration')
+    let kolom           = "'column1','column2','column3','column4','column5','column6','column7'"
+    let validasi        = "'column1'        => 'required'"
+    let data            = "'column1'        => $request->column1"
+    let data_comment    = "// 'column1'        => $request->column1"
+    let migrations      = "$table->string('column1')"
+    res_model.text(`<?php
+    
 namespace App;
     
 use Illuminate\\Database\\Eloquent\\Model;
     
-class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Model{
-    protected $table = "${table.val().toLowerCase().replace(" ", "_")}";
+class ${table} extends Model{
+    protected $table = "${table.toLowerCase().replace(" ", "_")}";
 
     protected $fillable = [${kolom}];
+
+    // public $timestamps = false;
 
     /**
      * Create by LeeNuksID :D
@@ -75,17 +266,14 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Mod
      * Thanks For Using Laragen
      */
 }`)
-    res_crud.text(`<?php
-
-
+        res_crud.text(`<?php
+    
 namespace App\\Http\\Controllers;
 
 use Illuminate\\Http\\Request;
-use App\\${table.val().charAt(0).toUpperCase() + table.val().substr(1)};
-use Illuminate\\Support\\Facades\\Validator;
-// use Illuminate\\Support\\Facades\\Auth;
+use App\\${table};
 
-class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller extends Controller{
+class ${table}Controller extends Controller{
 
     public function store(Request $request){
         $validate = Validator::make($request->all(),[
@@ -96,15 +284,15 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller e
             return redirect()->back()->withErrors($validate);
         }
 
-        $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create([
+        $send = ${table.charAt(0).toUpperCase() + table.substr(1)}::create([
             ${data}
         ]);
 
-        // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::insert([
+        // $send = ${table.charAt(0).toUpperCase() + table.substr(1)}::insert([
         ${data_comment}
         // ]);
         
-        // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create($request->all());
+        // $send = ${table.charAt(0).toUpperCase() + table.substr(1)}::create($request->all());
 
 
         if ($send) {
@@ -112,7 +300,7 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller e
         }
     }
 
-    public function update(Request $request,$${table.val().toLowerCase()}){
+    public function update(Request $request,$${table.toLowerCase()}){
         $validate = Validator::make($request->all(),[
             ${validasi}
         ]);
@@ -121,46 +309,46 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller e
             return redirect()->back()->withErrors($validate);
         }
 
-        $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
+        $${table.toLowerCase()} = ${table.charAt(0).toUpperCase() + table.substr(1)}::find($${table.toLowerCase()});
 
         $array = [
             ${data}
         ];
 
-        if ($${table.val().toLowerCase()}->update($array)) {
+        if ($${table.toLowerCase()}->update($array)) {
             return redirect()->back();
         }
     }
 
-    public function destroy($${table.val().toLowerCase()}){
-        $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
-        if ($${table.val().toLowerCase()}->delete()) {
+    public function destroy($${table.toLowerCase()}){
+        $${table.toLowerCase()} = ${table.charAt(0).toUpperCase() + table.substr(1)}::find($${table.toLowerCase()});
+        if ($${table.toLowerCase()}->delete()) {
             return redirect()->back();
         }
     }
-
+    
     /**
      * Create by LeeNuksID :D
      *
      * Thanks For Using Laragen
      */
 }`)
-    res_migration.text(`<?php
-
+        res_migration.text(`<?php
+    
 use Illuminate\\Database\\Migrations\\Migration;
 use Illuminate\\Database\\Schema\\Blueprint;
 use Illuminate\\Support\\Facades\\Schema;
 
-class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Migration{
+class ${table} extends Migration{
     
     public function up(){
-        Schema::create('${table.val().toLowerCase().replace(" ", "_")}', function (Blueprint $table) {
+        Schema::create('${table.toLowerCase().replace(" ", "_")}', function (Blueprint $table) {
             
-            $table->bigIncrements('id');
+            $table->id();
             ${migrations}
             $table->timestamps();
 
-            // Options: See More at https://laravel.com/docs/5.8/migrations#creating-columns
+            // Options: See More at https://laravel.com/docs/7.x/migrations#creating-columns
             // integer
             // bigInteger
             // binary
@@ -177,7 +365,7 @@ class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Mig
     }
 
     public function down(){
-        Schema::dropIfExists('${table.val().toLowerCase().replace(" ", "_")}');
+        Schema::dropIfExists('${table.toLowerCase().replace(" ", "_")}');
     }
 
     /**
