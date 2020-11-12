c99ab6ccfdad81497956aff2a0c1fbc69efb05fc
aibnuhibban <abd.ibnuhibban@gmail.com>
2020-11-12 15:59:32 +0700

    beauty code

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,133 +124,132 @@ function generate(){
         }
         res_model.text(`<?php
 
-    namespace App\\Models;
+namespace App\\Models;
+
+use Illuminate\\Database\\Eloquent\\Factories\\HasFactory;        
+use Illuminate\\Database\\Eloquent\\Model;
     
-    use Illuminate\\Database\\Eloquent\\Factories\\HasFactory;        
-    use Illuminate\\Database\\Eloquent\\Model;
-        
-    class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Model{
-        use HasFactory;
+class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Model{
+    use HasFactory;
 
-        protected $table = "${table.val().toLowerCase().replace(" ", "_")}";
+    protected $table = "${table.val().toLowerCase().replace(" ", "_")}";
 
-        protected $fillable = [${kolom}];
+    protected $fillable = [${kolom}];
 
-        /**
-         * Create by LeeNuksID :D
-         *
-         * Thanks For Using Laragen
-         */
-    }`)
+    /**
+     * Create by LeeNuksID :D
+     *
+     * Thanks For Using Laragen
+     */
+}`)
         res_crud.text(`<?php
 
-    namespace App\\Http\\Controllers;
+namespace App\\Http\\Controllers;
 
-    use Illuminate\\Http\\Request;
-    use App\\Models\\${table.val().charAt(0).toUpperCase() + table.val().substr(1)};
-    use Illuminate\\Support\\Facades\\Validator;
-    // use Illuminate\\Support\\Facades\\Auth;
+use Illuminate\\Http\\Request;
+use App\\Models\\${table.val().charAt(0).toUpperCase() + table.val().substr(1)};
+use Illuminate\\Support\\Facades\\Validator;
+// use Illuminate\\Support\\Facades\\Auth;
 
-    class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller extends Controller{
+class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}Controller extends Controller{
 
-        public function store(Request $request){
-            $validate = Validator::make($request->all(),[
-                ${validasi}
-            ]);
+    public function store(Request $request){
+        $validate = Validator::make($request->all(),[
+            ${validasi}
+        ]);
 
-            if ($validate->fails()) {
-                return redirect()->back()->withErrors($validate);
-            }
-
-            $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create([
-                ${data}
-            ]);
-
-            // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::insert([
-            ${data_comment}
-            // ]);
-            
-            // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create($request->all());
-
-
-            if ($send) {
-                return redirect()->back();
-            }
+        if ($validate->fails()) {
+            return redirect()->back()->withErrors($validate);
         }
 
-        public function update(Request $request,$${table.val().toLowerCase()}){
-            $validate = Validator::make($request->all(),[
-                ${validasi}
-            ]);
+        $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create([
+            ${data}
+        ]);
 
-            if ($validate->fails()) {
-                return redirect()->back()->withErrors($validate);
-            }
+        // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::insert([
+        ${data_comment}
+        // ]);
+        
+        // $send = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::create($request->all());
 
-            $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
 
-            $array = [
-                ${data}
-            ];
+        if ($send) {
+            return redirect()->back();
+        }
+    }
 
-            if ($${table.val().toLowerCase()}->update($array)) {
-                return redirect()->back();
-            }
+    public function update(Request $request,$${table.val().toLowerCase()}){
+        $validate = Validator::make($request->all(),[
+            ${validasi}
+        ]);
+
+        if ($validate->fails()) {
+            return redirect()->back()->withErrors($validate);
         }
 
-        public function destroy($${table.val().toLowerCase()}){
-            $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
-            if ($${table.val().toLowerCase()}->delete()) {
-                return redirect()->back();
-            }
-        }
+        $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
 
-        /**
-         * Create by LeeNuksID :D
-         *
-         * Thanks For Using Laragen
-         */
-    }`)
+        $array = [
+            ${data}
+        ];
+
+        if ($${table.val().toLowerCase()}->update($array)) {
+            return redirect()->back();
+        }
+    }
+
+    public function destroy($${table.val().toLowerCase()}){
+        $${table.val().toLowerCase()} = ${table.val().charAt(0).toUpperCase() + table.val().substr(1)}::find($${table.val().toLowerCase()});
+        if ($${table.val().toLowerCase()}->delete()) {
+            return redirect()->back();
+        }
+    }
+
+    /**
+     * Create by LeeNuksID :D
+     *
+     * Thanks For Using Laragen
+     */
+}`)
         res_migration.text(`<?php
 
-    use Illuminate\\Database\\Migrations\\Migration;
-    use Illuminate\\Database\\Schema\\Blueprint;
-    use Illuminate\\Support\\Facades\\Schema;
+use Illuminate\\Database\\Migrations\\Migration;
+use Illuminate\\Database\\Schema\\Blueprint;
+use Illuminate\\Support\\Facades\\Schema;
 
-    class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Migration{
-        
-        public function up(){
-            Schema::create('${table.val().toLowerCase().replace(" ", "_")}', function (Blueprint $table) {
-                
-                $table->bigIncrements('id');
-                ${migrations}
-                $table->timestamps();
+class ${table.val().charAt(0).toUpperCase() + table.val().substr(1)} extends Migration{
 
-                // Options: See More at https://laravel.com/docs/5.8/migrations#creating-columns
-                // integer
-                // bigInteger
-                // binary
-                // boolean
-                // char
-                // date
-                // dateTime
-                // decimal
-                // double
-                // enum('level', ['easy', 'hard']);
-                // float
-                // longText
-            });
-        }
+public function up(){
+    Schema::create('${table.val().toLowerCase().replace(" ", "_")}', function (Blueprint $table) {        
+        $table->id();
+        ${migrations}
+        $table->timestamps();
 
-        public function down(){
-            Schema::dropIfExists('${table.val().toLowerCase().replace(" ", "_")}');
-        }
+        // Options: See More at https://laravel.com/docs/5.8/migrations#creating-columns
+        // integer
+        // bigInteger
+        // binary
+        // boolean
+        // char
+        // date
+        // dateTime
+        // decimal
+        // double
+        // enum('level', ['easy', 'hard']);
+        // float
+        // longText
+    });
+}
 
-        /**
-         * Create by LeeNuksID :D
-         *
-         * Thanks For Using Laragen
-         */
+public function down(){
+    Schema::dropIfExists('${table.val().toLowerCase().replace(" ", "_")}');
+}
+
+/**
+ * Create by LeeNuksID :D
+ *
+ * Thanks For Using Laragen
+ */
     }`)
         
         cp_model.text('')
@@ -379,8 +378,7 @@ use Illuminate\\Support\\Facades\\Schema;
 class ${table} extends Migration{
     
     public function up(){
-        Schema::create('${table.toLowerCase().replace(" ", "_")}', function (Blueprint $table) {
-            
+        Schema::create('${table.toLowerCase().replace(" ", "_")}', function (Blueprint $table) {            
             $table->id();
             ${migrations}
             $table->timestamps();
